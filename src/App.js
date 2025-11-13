0d49dbc4d7d31258790777727c0db80945abd59b
ItaloGG9 <104172674+ItaloGG9@users.noreply.github.com>
2025-11-13 01:16:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "./pages/Home";
-import App from "./App";
 import Turnoindex from "./pages/Turno/Turnoindex";
 import Ventasindex from "./pages/Ventas/Ventasindex";
 import Adminindex from "./pages/admin/Adminindex";
@@ -15,47 +15,26 @@ import CerrarDia from "./pages/admin/CerrarDia";
 export default function App() {
   return (
     <Routes>
-      {/* Tu home u otras rutas */}
+      {/* Redirige la raíz a /pagos */}
       <Route path="/" element={<Navigate to="/pagos" replace />} />
 
-      {/* 👉 Aquí registras la pantalla de ventas en /pagos */}
+      {/* Pagos/Ventas */}
       <Route path="/pagos" element={<Ventasindex />} />
-
-      {/* (Opcional) Mantener compatibilidad con /ventas */}
       <Route path="/ventas" element={<Ventasindex />} />
 
-      {/* 404 (opcional) */}
+      {/* Rutas existentes */}
+      <Route path="/home" element={<Home />} />
+      <Route path="/turnos" element={<Turnoindex />} />
+      <Route path="/admin" element={<Adminindex />} />
+      <Route path="/turnos/abrir" element={<AbrirTurno />} />
+      <Route path="/turnos/cerrar" element={<CerrarTurno />} />
+      <Route path="/turnos/ver" element={<VerTurnos />} />
+      <Route path="/admin/ventas" element={<VentasTotales />} />
+      <Route path="/admin/turnos" element={<TurnosActivos />} />
+      <Route path="/admin/cerrar" element={<CerrarDia />} />
+
+      {/* 404 */}
       <Route path="*" element={<div>404</div>} />
     </Routes>
   );
 }
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-
-function App() {
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/turnos" element={<Turnoindex />} />
-        <Route path="/ventas" element={<Ventasindex />} />
-        <Route path="/admin" element={<Adminindex />} />
-        <Route path="/turnos/abrir" element={<AbrirTurno />} />
-        <Route path="/turnos/cerrar" element={<CerrarTurno />} />
-        <Route path="/turnos/ver" element={<VerTurnos />} />
-        <Route path="/admin/ventas" element={<VentasTotales />} />
-        <Route path="/admin/turnos" element={<TurnosActivos />} />
-        <Route path="/admin/cerrar" element={<CerrarDia />} />  
-
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;

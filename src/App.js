02397e45a1c6f3352bf6a91f97c3b5ed9238c460
ItaloGG9 <104172674+ItaloGG9@users.noreply.github.com>
2025-11-13 01:00:15 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import App from "./App";
 import Turnoindex from "./pages/Turno/Turnoindex";
 import Ventasindex from "./pages/Ventas/Ventasindex";
 import Adminindex from "./pages/admin/Adminindex";
@@ -11,7 +12,11 @@ import VentasTotales from "./pages/admin/VentasTotales";
 import TurnosActivos from "./pages/admin/TurnosActivos";
 import CerrarDia from "./pages/admin/CerrarDia";
 
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 
 function App() {
